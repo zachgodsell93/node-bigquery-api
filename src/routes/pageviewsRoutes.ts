edaab4dcea88a5cae315045c527b5b39dc808075
Zach Godsell <zacharyjamesgodsell@gmail.com>
2023-04-19 00:03:15 +1000

    update

--- src/routes/pageviewsRoutes.ts
+++ src/routes/pageviewsRoutes.ts
@@ -3,11 +3,11 @@ import { PageviewsController } from "../controllers/GaController";
 import bodyParser from "body-parser";
 
 const pageviewsRouter = express.Router();
-const pageviewsController = new PageviewsController();
+const pvController = new PageviewsController();
 pageviewsRouter.use(express.json());
 
 pageviewsRouter.get("/", async (req, res) => {
-	const pv = await pageviewsController.getPageViews();
+	const pv = await pvController.getPageViews();
 	res.json(pv);
 });
 
@@ -15,7 +15,7 @@ pageviewsRouter.get("/byDate", (req, res) => {
 	const startDate = new Date(req.body.startDate);
 	const endDate = new Date(req.body.endDate);
 
-	const pv = pageviewsController.getPageViewsBetweenDate(startDate, endDate); // Fixed typo here
+	const pv = pvController.getPageViewsBetweenDate(startDate, endDate); // Fixed typo here
 	if (pv) {
 		res.json(pv);
 	} else {
